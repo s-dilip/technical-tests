a41459d28ff0941291c5e8392ba69b4a519e9a8e
Swastik Dilip <justsyd224@gmail.com>
2022-11-04 12:57:23 +0000

    Implemented partial points calculation

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -1,1 +1,22 @@
+def calculate_points(word):
+
+
+    total_points = 0
+
+    for alpbt in word:
+
+        match alpbt:
+            case 'E' | 'A'| 'I'| 'O'| 'N'| 'R'| 'T'| 'L'| 'S'| 'U':
+                total_points += 1
+
+            case 'D'| 'G':
+                total_points +=2
+    
+
+    return total_points
+
+
+
 print("Ready to Go")
+print(calculate_points("EAI"))
+
