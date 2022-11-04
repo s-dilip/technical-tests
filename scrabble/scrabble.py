1ab200c8f17ae1bdec1a2fb33068ee8e3239de8c
Swastik Dilip <justsyd224@gmail.com>
2022-11-04 13:01:05 +0000

    Fully functional word points calculator

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -1,6 +1,5 @@
 def calculate_points(word):
 
-
     total_points = 0
 
     for alpbt in word:
@@ -8,15 +7,20 @@ def calculate_points(word):
         match alpbt:
             case 'E' | 'A'| 'I'| 'O'| 'N'| 'R'| 'T'| 'L'| 'S'| 'U':
                 total_points += 1
-
             case 'D'| 'G':
                 total_points +=2
+            case 'B'| 'C'| 'M'| 'P':
+                total_points +=3
+            case 'F'| 'H'|'V'| 'W'|'Y':
+                total_points +=4
+            case 'K':
+                total_points +=5
+            case 'J'| 'X':
+                total_points +=8
+            case 'Q'| 'Z':
+                total_points +=10
     
-
     return total_points
 
-
-
-print("Ready to Go")
-print(calculate_points("EAI"))
+print(calculate_points("GUARDIAN"))
 
